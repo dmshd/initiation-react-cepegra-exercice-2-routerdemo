ebba352a9aae1d295a562aae712934c57660a1a0
dmshd <daniel.muyshond@gmail.com>
2019-05-03 15:59:55 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+// 314a55e28f0e4e619fdafcbef75c8b22
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header> <Link to='/'>Section 1</Link> | <Link to='/2'>Section 2</Link> | <Link to='/3'>Section 3</Link></header>
+        <Switch>
+
+          <Route exact path="/" render={
+            ()=>(
+              <section>
+                <h2>SECTION 1</h2>
+              </section>
+            )
+          } />
+          
+          <Route exact path="/2" render={
+            ()=>(
+              <section>
+                <h2>SECTION 2</h2>
+              </section>
+            )
+          } />
+
+
+          <Route exact path="/3" render={
+            ()=>(
+              <section>
+                <h2>SECTION 3</h2>
+              </section>
+            )
+          } />
+        </Switch>
+        </Router>
     </div>
   );
 }
